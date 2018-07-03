65b18baf9551e5ab9da036f92afe006c4632a8bf
Jeffrey Penkar <jspenkar@gmail.com>
2018-07-02 23:15:06 -0500

    Webpack & package updates.

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,29 @@
 var path = require('path');
-var webpack = require('webpack');
-
 module.exports = {
-  context: __dirname,
-  entry: {
-    'bundle.js':'./src/index.js',
+  context:__dirname,
+  output:{
+    path:path.resolve(__dirname,'dist'),
+    filename:'bundle.js'
   },
-  output: {
-    path: __dirname,
-    filename: './dist/[name]'
-  },
-  module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [
-        "style-loader",
-        "css-loader",
-        "sass-loader"
-      ]
+  module:{
+    rules:[{
+      exclude:/node_modules/,
+      test:/\.scss$/,
+      use:["style-loader","css-loader","sass-loader"]
     },{
       test:/\.ttf/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'url-loader'
-      }
+      exclude:/node_modules/,
+      use:{ loader:'url-loader' }
     },{
-      test: /\.html$/,
-      exclude: /(node_modules|bower_components)/,
+      test:/\.js$/,
+      exclude:/node_modules/,
       use: {
-        loader: 'html-loader'
-      }
-    },{
-      test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader',
+        loader:'babel-loader',
         query: {
           presets: ['react', 'es2015', 'stage-0']
         },
       }
     }]
   },
-  devtool: 'source-map',
-  watch: true
+  devtool:'source-map'
 }
