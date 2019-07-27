3bd6f7f14d29942ace3776e3bb5c201aa1c67bd4
JSPenkar <jspenkar@gmail.com>
2019-07-27 18:51:31 -0500

    Article column would be more evenly stacked if more articles were present on page.

--- src/js/Components/HomePage/HomePageBody.js
+++ src/js/Components/HomePage/HomePageBody.js
@@ -3,14 +3,22 @@ import cn from 'classnames';
 import {StoryTeaser} from './StoryTeaser';
 
 const HomePageBody = (stories) => {
-  let arr1 = [], arr2 = [], arr3 = [];
-  stories.map((item, i) => {
-    let j = i % 3;
-    if(!j) {
+  let arr1 = [], arr2 = [], arr3 = [], arr1Sum = 0, arr2Sum = 0, arr3Sum = 0;
+  stories.forEach((item, i) => {
+    if(arr1Sum <= arr2Sum) {
+      item.story.slice(0,3).forEach((tease) => {
+        arr1Sum += (tease.text || "").length;
+      });
       arr1.push(StoryTeaser(item, i));
-    } else if (j === 1) {
+    } else if(arr2Sum <= arr3Sum) {
+      item.story.slice(0,3).forEach((tease) => {
+        arr2Sum += (tease.text || "").length;
+      });
       arr2.push(StoryTeaser(item, i));
     } else {
+      item.story.slice(0,3).forEach((tease) => {
+        arr3Sum += (tease.text || "").length;
+      });
       arr3.push(StoryTeaser(item, i));
     }
   });
