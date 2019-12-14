90dfd5f2f537621a27054bef5acb551174a05c89
Jeffrey Penkar <jspenkar@gmail.com>
2019-12-14 11:57:19 -0600

    MainArticle components updated.

--- src/js/Components/MainArticle/MainArticle.js
+++ src/js/Components/MainArticle/MainArticle.js
@@ -1,24 +1,27 @@
 import React from 'react';
+import { string, array } from 'prop-types';
 import StoryElement from '../HomePage/StoryElement';
 import cn from 'classnames';
 
-const MainArticle = (props) => {
+const MainArticle = ({ eClass, title, author, story = [] }) => {
+  const componentClass = cn('story-teaser-component', 'main-article-component', eClass);
   return (
     <div className='home-page-body'>
-      <div className={cn('story-teaser-component', 'main-article-component', props.eClass)}>
-        { props.title && <div className='title'>{ props.title }</div> }
-        { props.author && <div className='author'>{ props.author.join(', ') }</div> }
+      <div className={componentClass}>
+        { title && <div className='title'>{ title }</div> }
+        { author && <div className='author'>{ author.join(', ') }</div> }
 
-        { props.story.map((str, j) => (StoryElement(str, j))) }
+        { story.map((str, j) => (StoryElement(str, j))) }
       </div>
     </div>
-  )
+  );
 }
-MainArticle.defaultProps = {
-  title:'',
-  author:'',
-  story: [],
-  eClass:"",
+
+MainArticle.propTypes = {
+  title: string,
+  author: string,
+  story: array,
+  eClass: string,
 }
 
 export {MainArticle}
